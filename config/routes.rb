0e874cf825adcdddbd7314704142b7bafe741d6d
ozumaops <ozumaops@yahoo.co.jp>
2024-07-14 19:17:31 +0900

    ユーザー認証

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
+  root 'users#new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :users, only: [:show]
 end
